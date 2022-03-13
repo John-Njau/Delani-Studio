3e2a16a5d66f1ca053206769936e2cdb282f2c3c
John <john.njoroge@student.moringaschool.com>
2022-03-13 17:32:18 +0300

    ft: Added form validation code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// jQuery
 //  toggle
 $().ready(function () {
     $("#design").click(function () {
@@ -21,4 +22,34 @@ $().ready(function () {
     });
 });
 
-// FORM
+
+// Javascript
+// form
+document.getElementById("submitBtn").addEventListener('click', submitForm)
+
+function submitForm() {
+    var name = document.forms['contactForm']['name'].value;
+    var email = document.forms['contactForm']['email'].value;
+    var message = document.forms['contactForm']['message'].value;
+    var special=/^[^*|\":<>[\]{}`\\()';@&$]+$/;
+
+    var userArray = [];
+
+    if (name === "" && name == special) {
+        alert("Please enter Your name");
+        name.focus()
+    }
+    else if (email === "") {
+        alert('Enter Your Email')
+    }
+    else if (message === '') {
+        alert("Message too short.")
+    }
+
+    else {
+        alert(`${name}, we have received your message. Thank you for reaching us. Our Agents will respond to you Asap.`)
+    }
+
+    userArray.push(name)
+    console.log(userArray);
+};
